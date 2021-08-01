622fe02b38f54d6841da21eb2865a83154488b15
Easwaran-T-H <easwaran.subrama@tigeranalytics.com>
2021-08-01 13:48:10 +0530

    setup.py update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DS_Repos
 For storing Data Science related modules and other files.
+
+##  tools_
+Module(s) to be used as package
--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+
+setup(
     name='tools_',
-    version='21.1.1',
+    version='0.0.1',
     author='Easwaran T H',
     author_email='hariharaneaswaran@gmail.com',
     description='Tools for EDA and ML',
+    long_description = readme(),
+    long_description_content_type='text/markdown',
+    classifiers =[
+                    'Development Status :: 5 - Production/Stable',
+                    'License :: OSI Approved :: MIT License',
+                    'Programming Language :: Python :: 3',
+                    'Operating System :: OS Independent'
+                 ],
+
+
     url='https://github.com/Eashwar-22/DS_Repos/tree/eda',
-    packages=['tools_'],
-    install_requires=['requests'],
+    keywords = 'eda',
+    license = 'MIT',
+
+    packages='find',
+    install_requires=[],
+    include_package_data = True,
+    zip_safe = False
 )
